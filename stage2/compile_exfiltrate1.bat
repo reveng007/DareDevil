3dfe2c44d48c4baf365f8571e2bb1812d05233aa
Soumyani1 <soumyanilbiswas2018@gmail.com>
2022-08-29 03:52:14 +0530

    Update and rename compile_remotewrite.bat to compile_exfiltrate1.bat

---
+++ stage2/compile_exfiltrate1.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+csc.exe /target:exe /platform:x64 /out:exfiltrate1.exe  .\exfiltrate1.cs
--- stage2/compile_remotewrite.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-csc.exe /target:exe /platform:x64 /out:remotewrite.exe  .\remotewrite.cs
